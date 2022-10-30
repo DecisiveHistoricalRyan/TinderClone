a10197d74d2d1810601353a4edf4fe1aa7bb34b1
jason.cho <jason.cho@freedgrouptech.com>
2022-10-31 01:31:19 +0900

    change column names to be more detailed

--- app/domain/models.py
+++ app/domain/models.py
@@ -18,11 +18,12 @@ class User:
     photo: list = field(default_factory=list)
     description: str = ""
 
-    likes_on: list = field(default_factory=list)  # Mapped
-    liked_by: list = field(default_factory=list)
+    # self means the user of a row
+    users_liked_by_self: list = field(default_factory=list)  # Mapped
+    users_who_like_self: list = field(default_factory=list)
 
-    dislikes_on: list = field(default_factory=list)
-    disliked_by: list = field(default_factory=list)
+    users_disliked_by_self: list = field(default_factory=list)
+    users_who_dislike_self: list = field(default_factory=list)
 
     create_dt: datetime = field(init=False)
     update_dt: datetime = field(init=False)
