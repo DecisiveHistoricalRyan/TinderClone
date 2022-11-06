72b429c128b30765934b9a6db329ae097a9f6a97
migo <migo@migos-MacBook-Pro.local>
2022-11-07 00:09:49 +0900

    Huge Change on layerings

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,7 +1,7 @@
 from collections import deque
 from dataclasses import dataclass, field
 from datetime import datetime
-from uuid import UUID
+from uuid import UUID, uuid4
 
 
 @dataclass
@@ -11,12 +11,12 @@ class User:
     name: str
     age: int  # Index
     gender: str  # Index
-    school: str | None
-    job: str | None
     phone: str
     email: str
     photo: list = field(default_factory=list)
     description: str = ""
+    school: str | None = None
+    job: str | None = None
 
     # self means the user of a row
     users_liked_by_self: list["User"] = field(default_factory=list)  # Mapped
@@ -28,3 +28,7 @@ class User:
     create_dt: datetime = field(init=False)
     update_dt: datetime = field(init=False)
     events: deque = deque()
+
+    @classmethod
+    def create_user(cls, **kwargs):
+        return cls(id=uuid4(), **kwargs)
