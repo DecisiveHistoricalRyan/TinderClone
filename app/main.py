cb8f2c26c321a1fadfd27a43a0f2ccb706023a66
Ben Song <ben.song@freedgrouptech.com>
2022-11-25 00:42:42 +0900

    added endpoints

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.config import settings
+from app.entrypoints.api import router
+
+
+app = FastAPI()
+
+app.include_router(router)
+
+
+if __name__ == '__main__':
+    debug = settings.STAGE
+    uvicorn.run("app.main:app", port=8082, reload=True)
