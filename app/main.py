f57ea77fa38eb49e92e4ed0bdcaafda806517370
jason.cho <jason.cho@freedgrouptech.com>
2022-11-27 23:08:04 +0900

    mypy errors fixed

--- app/main.py
+++ app/main.py
@@ -4,12 +4,11 @@ from fastapi import FastAPI
 from app.config import settings
 from app.entrypoints.api import router
 
-
 app = FastAPI()
 
 app.include_router(router)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     debug = settings.STAGE
     uvicorn.run("app.main:app", port=8082, reload=True)
